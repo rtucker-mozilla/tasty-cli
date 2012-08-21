638aa5a379bc954d0ae7c5898c2c002bade4ddd1
Rob Tucker <rtucker@mozilla.com>
2012-08-21 09:35:38 -0400

    Updates to use locally cached files for speed

--- api_test.py
+++ api_test.py
@@ -1,24 +1,44 @@
 #!/usr/bin/python
-import sys
+import sys, os
 import argparse
 from MozInventoryCLI import MozInventoryCLI
+import pickle
+pickle_file = "%s/.tasty-pickle" % os.path.expanduser('~')
+pickle_file_expiration = 3600
+native_args = [
+        'create',
+        'delete',
+        'update',
+        'search',
+        'read',
+        ]
 
-if __name__ == '__main__':
+m = MozInventoryCLI()
 
-    m = MozInventoryCLI()
-    # create the top-level parser
+class PickledResponse(object):
+    namespaces = {}
+    def __init__(self, ns):
+        self.namespaces = ns
+
+    def list_namespaces(self):
+        return self.namespaces
+
+    def schema(self, ns):
+        return self.namespaces[ns]
+
+def refresh():
+    return build_response(should_pickle=True, moz_inv_object=m)
+
+def build_response(should_pickle=True, moz_inv_object=None):
+    if not should_pickle:
+        pickled_response = pickle.load( open( pickle_file, "rb" ) )
+        m = PickledResponse(pickled_response)
+    else:
+        m = moz_inv_object
+
     parser = argparse.ArgumentParser(prog='PROG')
-    #parser.add_argument('--foo', action='store_true', help='foo help')
     subparsers = parser.add_subparsers(dest='command')
-    # create the parser for the "a" command
-    namespaces = m.list_namespaces()
-    native_args = [
-            'create',
-            'delete',
-            'update',
-            'search',
-            'read',
-            ]
+    namespaces = {}
     for ns in m.list_namespaces().iterkeys():
         tmp = subparsers.add_parser(ns, help='api access to %s' % ns,)
         tmp.add_argument('--create', help='ACTION: create %s' % ns, action='store_true')
@@ -29,13 +49,37 @@ if __name__ == '__main__':
         if '--create' not in sys.argv and '--delete' not in sys.argv and '--search' not in sys.argv:
             tmp.add_argument('argument__', help='OBJECT to act upon', action='store')
         schema = m.schema(ns)
-
+        if should_pickle:
+            namespaces[ns] = schema
         for sc in schema['fields'].iterkeys():
             tmp.add_argument(
                 '--%s' % sc,
-                #type=string,
                 help=schema['fields'][sc]['help_text'],
                 )
+    if should_pickle:
+        pickle.dump( namespaces, open( pickle_file, "wb" ) )
+    return parser
+
+def pickled():
+    return build_response(False)
+
+def file_age_in_seconds(pathname):
+    import time, stat
+    return time.time() - os.stat(pathname)[stat.ST_MTIME]
+
+def load_args():
+    if not os.path.exists(pickle_file):
+        return refresh()
+
+    if file_age_in_seconds(pickle_file) > pickle_file_expiration:
+        return refresh()
+    else:
+        return pickled()
+
+if __name__ == '__main__':
+
+    parser = load_args()
+
     cmd = parser.parse_args()
     action_dict = {}
     for na in cmd.__dict__:
@@ -46,7 +90,7 @@ if __name__ == '__main__':
         m.update(cmd.command, cmd.__dict__['argument__'], action_dict)
 
     if cmd.read:
-            m.read(cmd.command, cmd.argument__)
+        m.read(cmd.command, cmd.argument__)
 
     if cmd.create:
         m.create(cmd.command, action_dict)
